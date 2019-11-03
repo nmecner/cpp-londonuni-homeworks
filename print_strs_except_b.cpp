bbdaefb7fabfecc771a39e1e0f73271c9179cb21
Natalia <natalia.mecner@gmail.com>
2019-11-03 23:25:33 +0000

    Add program that prints strs that not begin with b from input

---
+++ print_strs_except_b.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<std::string> vec_str;
+	std::string st;
+	std::cout << "Would you kindly enter a string or type quit " << std::endl;
+	while (st != "quit") {
+		vec_str.push_back(st);
+		std::cin >> st;
+	}
+		
+	for (std::string s: vec_str) {
+		if (s[0] != 'b') {
+			std::cout << s << std::endl;
+		}
+	}
+	}
